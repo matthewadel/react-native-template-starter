eb497819d32558934d7df296b11c88cff80f1c14
matthewadel <matthewadel69@yahoo.com>
2021-03-17 13:32:36 +0200

    edit useLanuage hooks

--- template/src/Lang/useLanguage.ts
+++ template/src/Lang/useLanguage.ts
@@ -1,14 +1,7 @@
 import React from "react";
-import I18n from "react-native-i18n";
+import LocalizationContext from "./i18n";
 
 export const useLanguage = () => {
-
-  let x: "ar" | "en" = 'en'
-  const LocalizationContext = React.createContext({
-    t: (key: string,): string => I18n.t(key),
-    setLocale: (x: string): any => x,
-    locale: x
-  });
 
   let { t, locale, setLocale } = React.useContext(LocalizationContext);
 
