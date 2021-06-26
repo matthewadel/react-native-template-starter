3c8f151f880f28a00b2d97a1201eef5d83b97168
matthewadel <matthewadel69@yahoo.com>
2021-06-26 11:35:52 +0200

    fix lang folder

---
+++ template/src/lang/i18n.ts
@@ -0,0 +1,21 @@
+import React from "react";
+import I18n from "react-native-i18n";
+
+I18n.fallbacks = true;
+I18n.translations = {
+    en: require("./en"),
+    ar: require("./ar")
+}
+
+export function t(params = '') {
+    return I18n.t(params);
+}
+
+let x: "ar" | "en" = 'en'
+const LocalizationContext = React.createContext({
+    t: (key: string, options?: any): string => key,
+    setLocale: (x: string): any => x,
+    locale: x
+});
+
+export default LocalizationContext;
