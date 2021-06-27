52f83d015a18b25bdf032996dcb48f5a67ba90ef
matthewadel <35105735+matthewadel@users.noreply.github.com>
2021-06-27 12:40:08 +0200

    change textInput.tsx

--- template/src/UI/TextInput/index.tsx
+++ template/src/UI/TextInput/index.tsx
@@ -1,108 +1,48 @@
+import { ITextInput } from 'models';
 import React, { useState } from 'react';
-import {
-  TextInput as RNTextInput,
-  TextInputProps,
-  TextStyle,
-} from 'react-native';
-import * as Animatable from 'react-native-animatable';
+import { TextInput as RNTextInput } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import * as Animatable from 'react-native-animatable';
+import { ChangeDirectionStyle, Colors, ConvertStyleToObject, SVGImage, Text, VectorIcons } from 'UI';
 import { useLanguage } from 'lang/useLanguage';
-import {
-  ChangeDirectionStyle,
-  Colors,
-  FONT_FAMILY,
-  VectorIcons,
-  ConvertStyleToObject,
-} from 'UI';
 
-interface textInputProps extends TextInputProps {
-  textInputStyle?: TextStyle | TextStyle[];
-  children?: any;
-  togglePasswordButton?: boolean;
-  noDirectionChange?: boolean;
-  showStyle?: boolean;
-  errStyle?: {
-    hasError: boolean;
-    defaultColor?: string;
-  };
-  changeDirecton?: boolean;
-}
-
-// const TextInput = (props: textInputProps) => {
-export const TextInput = React.forwardRef((props: textInputProps, ref: any) => {
+const TextInput = React.forwardRef((props: ITextInput, ref: any) => {
   let { style, textInputStyle, errStyle } = props;
 
-  const [showPassword, togglePassword] = useState(false);
-
+  const [showPassword, togglePassword] = useState(false)
   const { locale } = useLanguage()
 
   return (
-    <Animatable.View
-      animation={errStyle && errStyle.hasError ? 'shake' : ''}
-      style={[
-        ChangeDirectionStyle(([{ flexDirection: 'row', alignItems: 'center' }, ConvertStyleToObject(style)]), props.noDirectionChange, props.showStyle),
-        errStyle
-          ? {
-            color: errStyle.hasError
-              ? Colors().Text.Error
-              : errStyle.defaultColor,
-            borderColor: errStyle.hasError
-              ? Colors().Text.Error
-              : errStyle.defaultColor,
-          }
-          : {},
-      ]}>
-      
-      {props.children}
-      
-      <RNTextInput
-        ref={ref}
-        autoCapitalize='none'
-        autoCorrect={false}
-        spellCheck={false}
-        placeholderTextColor={
-          errStyle
-            ? errStyle.hasError
-              ? Colors().Text.Error
-              : errStyle.defaultColor
-            : undefined
-        }
-        secureTextEntry={!!props.togglePasswordButton && !showPassword}
-        {...props}
-        style={[
-          ChangeDirectionStyle([{
-            textAlign: locale == 'en' ? 'left' : 'right',
-            fontSize: RFValue(16),
-            flex: 1,
-            height: '100%',
-            padding: 0,
-            fontFamily: FONT_FAMILY(),
-            paddingRight: !!props.togglePasswordButton ? '15%' : RFValue(10),
-            paddingLeft: !!(props.children) ? 0 : !!props.togglePasswordButton ? '15%' : RFValue(10),
-          },
-            ConvertStyleToObject(textInputStyle)]),
-        ]}
-      />
+    <>
 
-      {props.togglePasswordButton && (
-        <VectorIcons
-          noDirectionChange
-          style={{
-            width: '15%',
-            height: '100%',
-            position: 'absolute',
-            alignSelf: 'center',
-            right: 0,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          onPress={() => togglePassword((prev) => !prev)}
-          icon="Feather"
-          name={showPassword ? 'eye' : 'eye-off'}
-          size={RFValue(18)}
-          color={Colors().Text.Dark}
+      {!!props.label && <Text style={[{ marginVertical: RFValue(14), width: '100%' }, props.labelStyle]}>{props.label}</Text>}
+
+      <Animatable.View
+        animation={errStyle && errStyle.hasError ? 'shake' : ''}
+        style={ChangeDirectionStyle([
+          { flexDirection: 'row', alignItems: 'center' },
+          ChangeDirectionStyle(style, props.noDirectionChange),
+          errStyle ? {
+            color: errStyle.hasError ? Colors().Text.Error : errStyle.defaultColor,
+            borderColor: errStyle.hasError ? Colors().Text.Error : errStyle.defaultColor,
+          } : {},
+        ])}>
+
+        {!!props.svgSource && <SVGImage source={props.svgSource} width='70%' height='60%' style={{ borderTopLeftRadius: RFValue(10), borderBottomLeftRadius: RFValue(10), height: '100%', width: RFValue(50), backgroundColor: Colors().App.White }} />}
+
+        <RNTextInput
+          ref={ref}
+          placeholderTextColor={errStyle ? (errStyle.hasError ? Colors().Text.Error : errStyle.defaultColor) : undefined}
+          secureTextEntry={!!props.togglePasswordButton && !showPassword}
+          {...props}
+          style={[{ textAlign: locale == 'ar' ? 'right' : 'left', fontSize: RFValue(16), flex: 1, height: '100%', paddingVertical: 0, paddingHorizontal: !!props.togglePasswordButton ? '15%' : 0 }, ConvertStyleToObject(textInputStyle),]}
         />
-      )}
-    </Animatable.View>
+
+        {props.togglePasswordButton && <VectorIcons noDirectionChange style={{ width: '15%', height: '100%', position: 'absolute', alignSelf: 'center', right: 0, justifyContent: 'center', alignItems: 'center' }} onPress={() => togglePassword(prev => !prev)} icon="Feather" name={showPassword ? "eye" : "eye-off"} size={RFValue(22)} color={Colors().Text.Primary} />}
+
+      </Animatable.View>
+    </>
   );
-});
+})
+
+export { TextInput };
