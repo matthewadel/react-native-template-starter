f788b45a0b3f176b7b37d656c2f4851696565c45
matthewadel <35105735+matthewadel@users.noreply.github.com>
2021-07-18 19:18:03 +0200

    apply screen animations and gestures to android

--- template/src/navigation/AppStack.tsx
+++ template/src/navigation/AppStack.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator,CardStyleInterpolators} from '@react-navigation/stack';
 import {Welcome} from 'screens';
 
 const Stack = createStackNavigator();
 
 export default function RootStack() {
   return (
-    <Stack.Navigator initialRouteName="Welcome" headerMode="none">
+    <Stack.Navigator
+       screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: 'horizontal',
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+      }}
+      initialRouteName="Welcome" headerMode="none">
       <Stack.Screen name="Welcome" component={Welcome} />
     </Stack.Navigator>
   );
