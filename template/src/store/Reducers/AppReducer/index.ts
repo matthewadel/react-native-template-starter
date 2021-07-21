6115024032273c4fe3bf54067dd51f107867a0d9
matthewadel <35105735+matthewadel@users.noreply.github.com>
2021-07-21 18:58:30 +0200

    change app reducer to accept top and bottom notch

--- template/src/store/Reducers/AppReducer/index.ts
+++ template/src/store/Reducers/AppReducer/index.ts
@@ -2,12 +2,18 @@ import { SAVE_LANG, SET_NOTCH_HEIGHT } from 'store/Actions'
 
 export interface IApp {
   lang?: string
-  notch?: number | null
+  notch?:  {
+    top: number | null
+    bottom: number | null
+  }
 }
 
 const initialState: IApp = {
   lang: 'en',
-  notch: null,
+  notch:  {
+    top: number | null
+    bottom: number | null
+  },
 }
 
 const TextReducer = (state = initialState, action: any) => {
