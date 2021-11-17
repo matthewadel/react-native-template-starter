75e7e2ebdb75a2c0b2d51b231a0e7697d4d4f79f
matthewadel <matthewadel69@yahoo.com>
2021-11-17 21:20:44 +0200

    adding fbsdk and support for app tracking

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react';
-import { ActivityIndicator, View } from 'react-native'
+import { ActivityIndicator, View, Platform, } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppStack from './navigation/AppStack';
 import { store, persistor } from 'store';
-// import { SetNotchHeight } from 'store/Actions';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-// import SafeArea from 'react-native-safe-area';
 import reactotron from 'utils/Reactron';
 import I18n from "react-native-i18n";
 import LocalizationContext from 'lang/i18n';
 import Orientation from 'react-native-orientation';
 import FlashMessage from "react-native-flash-message";
 import { FlashMsg, Modal, ModalRef } from 'UI';
-// import { SetNotchHeight } from 'store/Actions';
+import { PERMISSIONS, RESULTS, request, check } from 'react-native-permissions';
+import { Settings } from 'react-native-fbsdk-next';
 import messaging from '@react-native-firebase/messaging';
 import PushNotification from "react-native-push-notification";
 
@@ -38,16 +37,7 @@ const App = () => {
 
   useEffect(() => {
 
-    //1- setting the notch
-    // const getNotchHeight = async () => {
-    //   let result = await SafeArea.getSafeAreaInsetsForRootView()
-    //  store.dispatch(SetNotchHeight({ top: (result.safeAreaInsets.top || 0), bottom: (result.safeAreaInsets.bottom || 0) }))
-    // }
-
-    // if (store.getState().App.notch.top === null)
-    //   getNotchHeight()
-
-    // 2- setting the language
+    // 1- setting the language
     setTimeout(() => {
       I18n.locale = store.getState().App.lang
       setLocale(store.getState().App.lang)
@@ -55,10 +45,35 @@ const App = () => {
 
     Orientation.lockToPortrait();
 
-    // 3- notifications request and set local notifications
+    // 2- notifications request and set local notifications
     requestNotificationsPermission()
 
+    // 3- request to collect data and init fbsdk
+    initFBSdk()
+
   }, [])
+
+  const initFBSdk = async () => {
+    if (Platform.OS === 'ios') {
+      const ATT_CHECK = await check(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
+
+      if (ATT_CHECK === RESULTS.DENIED) {
+        console.log('RESULTS.DENIED')
+        try {
+          const ATT = await request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
+          if (ATT === RESULTS.GRANTED) {
+            console.log('RESULTS.GRANTED')
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+
+    Settings.initializeSDK();
+    Settings.setAdvertiserTrackingEnabled(true);
+
+  }
 
   const requestNotificationsPermission = async () => {
     const authStatus = await messaging().requestPermission();
