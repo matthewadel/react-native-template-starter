ef44196af427ab38910b8042d090310d5687f222
matthewadel <matthewadel69@yahoo.com>
2023-03-27 11:49:32 +0200

    change checkbox component

--- template/src/UI/CheckBox/index.tsx
+++ template/src/UI/CheckBox/index.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { StyleProp, TextStyle, ViewStyle } from 'react-native'
 import RNCheckbox from 'react-native-checkbox-animated'
 import { RFValue, Colors } from 'UI'
+import * as Animatable from 'react-native-animatable';
+import { store } from 'store';
 
 interface ICheckBox {
   label?: string
   checkedBackgroundColor?: string
   unCheckedBorderColor?: string
   checked: boolean
-  onValueChange?: (val: boolean) => boolean
+  hasError?: boolean
+  onValueChange?: Function
   size?: number
+  labelContainerStyle?: any
   labelStyle?: StyleProp<TextStyle>
   checkStyle?: StyleProp<TextStyle>
   containerStyle?: StyleProp<ViewStyle>
@@ -20,23 +24,25 @@ interface ICheckBox {
 const CheckBox = (props: ICheckBox) => {
 
   return (
-    <RNCheckbox
-      label={props.label || ''}
-      checked={props.checked}
-      onValueChange={(x: boolean) => props.onValueChange ? props.onValueChange(x) : false}
-      checkedBackgroundColor={props.checkedBackgroundColor}
-      unCheckedBorderColor={props.unCheckedBorderColor}
-      rounded
-      size={props.size}
-      checkMarkSize={props.checkMarkSize || RFValue(20)}
-      labelContainerStyle={{ flex: 0, }}
-      labelStyle={[{ padding: 0 }, props.labelStyle]}
-      checkboxContainerStyle={{ padding: 0, }}
-      containerStyle={[{ alignItems: 'center', }, props.containerStyle]}
-      checkStyle={[{ color: Colors().Text.White, fontSize: RFValue(18), }, props.checkStyle]}
-      customMarker={props.customMarker}
-      borderWidth={props.checked ? 1 : 5}
-    />
+    <Animatable.View style={[{ alignItems: 'center', }, props.containerStyle]} animation={props.hasError ? 'shake' : ''}>
+      <RNCheckbox
+        label={props.label || ''}
+        containerStyle={{ flexDirection: store.getState().App.lang == 'ar' ? 'row-reverse' : 'row' }}
+        checked={props.checked}
+        onValueChange={(x: boolean) => props.onValueChange ? props.onValueChange(x) : false}
+        // checkedBackgroundColor={props.checkedBackgroundColor || Colors().App.backgroundColor}
+        // unCheckedBackgroundColor={props.checkedBackgroundColor || Colors().App.backgroundColor}
+        // unCheckedBorderColor={props.unCheckedBorderColor || Colors().App.backgroundColor}
+        checkedBorderColor={Colors().App.Primary}
+        size={props.size}
+        labelContainerStyle={[{ flex: 0, }, store.getState().App.lang == 'ar' ? { marginRight: RFValue(10) } : { marginLeft: RFValue(10) }, props.labelContainerStyle]}
+        labelStyle={[{ padding: 0, fontSize: RFValue(14) }, props.labelStyle]}
+        checkboxContainerStyle={{ padding: 0, }}
+        checkStyle={[{ color: Colors().Text.Primary, fontSize: RFValue(20), }, props.checkStyle]}
+        customMarker={props.customMarker}
+        boxStyle={{ width: RFValue(32), height: RFValue(32) }}
+      />
+    </Animatable.View>
   )
 
 }
