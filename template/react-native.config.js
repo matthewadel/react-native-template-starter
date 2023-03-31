0b2bf7b26036d6ee6d1bf78c88412264a9998864
matthewadel <matthewadel69@yahoo.com>
2023-03-31 23:06:54 +0200

    define custom path for ios

--- template/react-native.config.js
+++ template/react-native.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  project: {
+    ios: {
+      sourceDir: './ios',
+    },
+  },
   assets: ['./src/assets/Fonts/Cairo', './src/assets/Fonts/Poppins', './src/assets/Fonts/SFUI']
 }
